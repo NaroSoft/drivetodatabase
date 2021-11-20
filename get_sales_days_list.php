01441f3c3c1edfb31cddf61e5d432faf51373421
NaroSoft <76490425+NaroSoft@users.noreply.github.com>
2021-11-20 15:55:13 +0000

    Add files via upload

---
+++ get_sales_days_list.php
@@ -0,0 +1,21 @@
+<?php
+
+include 'conn.php';
+
+$date=$_POST['date'];
+
+
+        $sql = $conn->query("SELECT * FROM sales WHERE Date='".$date."' ORDER BY Time DESC");
+
+                $res = array();
+
+        while ($row=$sql->fetch_assoc()) {
+                $res[]=$row;
+            }
+            echo json_encode($res);
+
+
+?> 
+
+
+
