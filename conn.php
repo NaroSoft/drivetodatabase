aeb3ff72b12681d1cc1d46a0a095740f3dc9a827
NaroSoft <76490425+NaroSoft@users.noreply.github.com>
2021-11-20 13:48:26 +0000

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("remotemysql.com","qz5SfLgaac","jsRZm3bRN2","qz5SfLgaac");
+$mysqli = new mysqli("remotemysql.com","rCtGtsSc2X","7tN1qsJpsJ","rCtGtsSc2X");
 
 // Check connection
 if ($mysqli -> connect_errno) {
@@ -9,31 +9,5 @@ if ($mysqli -> connect_errno) {
   echo "Yep! Database Connected...";
 }
 
-// Gather all table names into an array.
-
-$query = "select * from demo";
-
-$result = $mysqli->query($query);
-print_r($result['name']);
-/*
-$tables = $result->fetch_all();
-
-// Step through the array, only accessing the first element (the table name)
-// and gather the column names in each table.
-foreach($tables as $table)
-{
-    echo "<h2>" . $table[0] . "</h2>";
-
-    $query = "DESCRIBE " . $table[0];
-    $result = $mysqli->query($query);
-
-    $columns = $result->fetch_all();
-
-    foreach($columns as $column)
-    {
-        echo $column[0] . "<br />";
-    }
-}
-*/
 $mysqli->close();
 ?>
